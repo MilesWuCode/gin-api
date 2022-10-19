a1af813da6760f71781b6883291884ce944ca300
Miles Wu <slion0725@gmail.com>
2022-10-19 02:48:50 +0000

    u

--- model/user.go
+++ model/user.go
@@ -2,21 +2,26 @@ package model
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type User struct {
-	// gorm field
-	gorm.Model
+	// gorm欄位
+	// gorm.Model
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 
-	// custom field
+	// 自訂義欄位
 	Name     string `gorm:"not null" json:"name" binding:"required"`
 	Email    string `gorm:"unique;not null" json:"email" binding:"required"`
 	Password string `gorm:"not null" json:"password" binding:"required"`
 
-	// relationships
-	// Posts    []Post `gorm:"foreignkey:UserID"`
+	// 自訂義關聯
+	// Posts []Post `gorm:"foreignkey:UserID"`
 }
 
 // table name
